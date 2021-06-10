342170b62a652e68d66521d68bf1253ff5c6d843
Vladimir Kirov <vladimir.kirov@siteground.com>
2021-06-10 22:14:41 +0300

    cleanup and replace IO layer with tac

--- tailsince.pl
+++ tailsince.pl
@@ -8,9 +8,8 @@ use diagnostics; # debugging
 use Data::Dumper;
 use Getopt::Long qw(HelpMessage VersionMessage);
 use Pod::Usage;
-use DateTime; # for time conversions and calculations
-use Env; # to cache timezone
-use open IN => ':reverse'; # for reading files in reverse
+use DateTime;              # for time conversions and calculations
+use Env;                   # to cache timezone
 
 my $now = DateTime->now;
 
@@ -18,7 +17,6 @@ my $now = DateTime->now;
 my $delta_value = '15';
 my $delta_unit = 'minutes';
 my @delta_units_list = qw/seconds minutes hours/;
-my $ipposition = 0;
 my %month_abbr = (
   'Jan' => '01',
   'Feb' => '02',
@@ -35,19 +33,14 @@ my %month_abbr = (
 );
 
 GetOptions(
-  'delta=i' => \$delta_value,
-  'timeunit=s' => \$delta_unit,
-  'iplimit=i' => \(my $iplimit = 10),
-  'scriptlimit=i' => \(my $scriptlimit = 10),
-  'vhostlimit=i' => \(my $vhostlimit = 10),
-  'help' => sub { HelpMessage(0) },
-  'version' => sub { VersionMessage(0) }
+  'delta=i'       => \$delta_value,
+  'timeunit=s'    => \$delta_unit,
+  'help'          => sub { HelpMessage(0) },
+  'version'       => sub { VersionMessage(0) }
   ) or HelpMessage(1);
 
-
-#my $reference = 1506496800;
 my $reference = DateTime->now->subtract( $delta_unit => $delta_value );
-print "$reference\n";
+# print "$reference\n";
 unless (@ARGV) {
   print "Need at least one file\n";
   HelpMessage(1);
@@ -59,60 +52,41 @@ unless ( $delta_unit =~ /seconds|minutes|hours/ ) {
 
 my @filelist = @ARGV;
 
-sub topips {
-  my (@toplist, %ip);
-  map $ip{(split)[$ipposition]}++, @_;
-  @toplist = map "$ip{$_} $_\n", sort {$ip{$b} <=> $ip{$a}} keys %ip;
-  my $topx = $iplimit - 1;
-  return @toplist[0..$topx]; # can this be shortened?
-}
-
 foreach my $file (@filelist) {
-  open(my $fh, "<", $file) or die "Could not open $file\n";
-  my (@timestamp, @fixedtimestamp, $epochtimestamp, @tail, %delta, @topxips);
+  open(my $fh, "-|","tac", $file) or die "Could not open $file\n";
+  my (@timestamp, @fixedtimestamp, $epochtimestamp, @tail, %delta);
   while (<$fh>) {
     @timestamp = split /[][]/; # remove square brackets from the timestamp
     @fixedtimestamp = split /\/|:| /, $timestamp[1];
-    $epochtimestamp = DateTime->new( year => $fixedtimestamp[2],
-                                      month => $month_abbr{"$fixedtimestamp[1]"},
-                                      day => $fixedtimestamp[0],
-                                      hour => $fixedtimestamp[3],
-                                      minute => $fixedtimestamp[4],
-                                      second => $fixedtimestamp[5],
-                                      time_zone => $fixedtimestamp[6],
+    $epochtimestamp = DateTime->new( year      => $fixedtimestamp[2],
+                                     month     => $month_abbr{"$fixedtimestamp[1]"},
+                                     day       => $fixedtimestamp[0],
+                                     hour      => $fixedtimestamp[3],
+                                     minute    => $fixedtimestamp[4],
+                                     second    => $fixedtimestamp[5],
+                                     time_zone => $fixedtimestamp[6],
                                      );
-   # print $reference->epoch . " " . $epochtimestamp->epoch,"\n";
   } continue {
     last if ( $epochtimestamp->epoch lt $reference->epoch );
     unshift @tail, $_;
     close ARGV if eof;
   }
-  #my @{$file} = <$fh>;
-  #unless (@{$file}tail) { die "No tail";}
-  #print @tail[0..2];
   print "@{$file}\n";
-  #my @g = @{grep {/\.php/} @tail};
-  #print @g;
-  #@t = &topips(grep {/\.php/} @tail);
-  #print @t;
 }
 
 __END__
 =head1 NAME
 
-sgan - analyze logs based
+tailsince - output the last part of file
 
 =head1 SYNOPSIS
 
   --delta,-d         Set time shift/delta backwards in time unit for analyzing the logs, integer, default is 15 (minutes)
   --timeunit,-t      Set the time unit for the lookback (default is minutes), can be seconds/minutes/hours
-  --iplimit,-i       Set the max top IPs (default is 10)
-  --scriptlimit,-s   Set the max top scripts (default is 10)
-  --vhostlimit,-v    Set the max top vhosts (default is 10)
   --help,-h          Print this help
 
 =head2 --version
 
-VERSION 0.02
+VERSION 0.1
 
 =cut
